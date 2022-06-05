9c7048f72e9aee36138d5ecb69cad320191263e9
Emilio Bravo <78387018+Emilio-Bravo@users.noreply.github.com>
2022-06-05 15:15:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const get = (target) => document.querySelector(target);
+
+window.addEventListener("DOMContentLoaded", () => {
+  get("#container").addEventListener("click", (e) => {
+    if (e.target.tagName == "LI") {
+      let target = get(e.target.getAttribute("linked-section")).offsetTop;
+
+      scroll({
+        behavior: "smooth",
+        top: target,
+      });
+    }
+  });
+});
